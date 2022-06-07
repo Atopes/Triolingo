dd29ff53f6f456429081f8d93642f66a19ce70a9
K0ZUME <unsung11224@gmail.com>
2022-06-07 20:58:42 +0200

    Update

--- Triolingo/app/src/main/java/com/example/triolingo/ProfileActivity.java
+++ Triolingo/app/src/main/java/com/example/triolingo/ProfileActivity.java
@@ -3,10 +3,7 @@ package com.example.triolingo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,26 +11,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
     private ImageView englishTest;
 
-    private FirebaseUser user;
-    private DatabaseReference reference;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
     private String userID;
     private Button logOut;
     public static int score = 0, level = 1;
     private TextView levelTxt, scoreTxt;
+    private ImageView profilePicMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +43,35 @@ public class ProfileActivity extends AppCompatActivity {
         englishTest = (ImageView) findViewById(R.id.englishTest);
         levelTxt = (TextView) findViewById(R.id.level);
         scoreTxt = (TextView) findViewById(R.id.score);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+
+        profilePicMain = findViewById(R.id.profilePicMain);
+
+        Query query = databaseReference.orderByChild("email").equalTo(firebaseUser.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+
+                    String image = "" + dataSnapshot1.child("image").getValue();
+
+                    try {
+                        Glide.with(ProfileActivity.this).load(image).into(profilePicMain);
+                    } catch (Exception ignored) {
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         logOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,13 +88,13 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("Users");
-        userID = user.getUid();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = firebaseUser.getUid();
 
         final TextView textViewWelcomeClient = (TextView) findViewById(R.id.userName);
 
-        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+        databaseReference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 User userProfile = snapshot.getValue(User.class);
@@ -90,7 +118,7 @@ public class ProfileActivity extends AppCompatActivity {
         UpdateStats();
     }
     public void UpdateStats(){
-        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+        databaseReference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 User userProfile = snapshot.getValue(User.class);
@@ -99,7 +127,7 @@ public class ProfileActivity extends AppCompatActivity {
                     Intent intent = getIntent();
                     score += intent.getIntExtra("score", 0);
 
-                    reference.child(userID).child("score").setValue(score);
+                    databaseReference.child(userID).child("score").setValue(score);
 
                     scoreTxt.setText("Your score: " + String.valueOf(score));
                     levelTxt.setText("Your level: " + String.valueOf(1 + (score/100)));
@@ -113,5 +141,9 @@ public class ProfileActivity extends AppCompatActivity {
     }
     public void engTestMenu(View view) {
         startActivity(new Intent(ProfileActivity.this, TestsMenu.class));
+    }
+
+    public void editProfile(View view) {
+        startActivity(new Intent(ProfileActivity.this, EditProfile.class));
     }
 }
