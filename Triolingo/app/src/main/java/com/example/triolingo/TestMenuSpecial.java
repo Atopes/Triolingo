abd26ebd6791f1d7263122eee99ad56a635148c9
K0ZUME <unsung11224@gmail.com>
2022-06-12 11:46:54 +0200

    Special tests

---
+++ Triolingo/app/src/main/java/com/example/triolingo/TestMenuSpecial.java
@@ -0,0 +1,14 @@
+package com.example.triolingo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestMenuSpecial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_menu_special);
+    }
+}
